ce8a4206578e7a340c701e72e04418f71ed95260
DanielGuillen88 <danielguillen85@gmail.com>
2025-07-18 17:52:01 +0200

    creado componente AboutMyLearning

---
+++ Mi-Portafolio/src/components/RightColumn/AboutMyLearning.jsx
@@ -0,0 +1,21 @@
+import Isdi from "./Isdi";
+import TheCorner from "./TheCorner";
+import Etif from "./Etif";
+
+export default function AboutMyLearning() {
+
+    return (
+        <>
+            <h3 className="section-title">Sobre mi aprendizaje</h3>
+
+            {/* # ISDI */}
+            <Isdi />
+            {/* # TheCorner */}
+            <TheCorner />
+            {/* # Etif */}
+            <Etif />
+
+        </>
+
+    );
+}
