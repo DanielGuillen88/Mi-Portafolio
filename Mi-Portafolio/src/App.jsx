521ff438a0250dd09a7df78d9c0d0b0a70351738
DanielGuillen88 <danielguillen85@gmail.com>
2025-07-22 09:43:19 +0200

    Añadidos estilos tailwind

--- Mi-Portafolio/src/App.jsx
+++ Mi-Portafolio/src/App.jsx
@@ -4,12 +4,18 @@ import RightColumn from './components/RightColumn';
 export default function App() {
 
   return (
-    <div className="cv-container">
-      
-      <LeftColumn/>
+    <div className="bg-white font-sans text-gray-800 p-5 max-w-screen-lg mx-auto min-h-screen">
+        <div className="shadow-xl rounded-lg overflow-hidden grid grid-cols-1 md:grid-cols-3 gap-8 bg-white">
 
-      <RightColumn/>
+          <div className="md:col-span-1">
+            <LeftColumn/>
+          </div>
 
+          <div className="md:col-span-2">
+            <RightColumn/>
+          </div>
+
+        </div>
     </div>
 
   )
